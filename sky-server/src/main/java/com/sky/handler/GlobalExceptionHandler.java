8bd175c6c29b746e25cd0432f1a1b8f1845bcbee
Ta321487 <tl_5099@163.com>
2025-09-19 09:22:54 +0800

    新增员工

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +27,18 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException sqlException){
+        String message = sqlException.getMessage();
+        if(message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            String userName = split[2];
+            String msg = userName + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else{
 
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+
+    }
 }
